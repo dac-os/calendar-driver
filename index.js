ec4be5934193f20d2b3c2ad45ab1a2133bcaf434
Lamasu <rafaerthal@gmail.com>
2014-08-26 20:49:18 -0300

    downloading all pages

--- index.js
+++ index.js
@@ -9,16 +9,38 @@ exports.configUri = function (newUri) {
   uri = newUri;
 };
 
-exports.calendars = function (page, next) {
+function downloadPages(url, next) {
   'use strict';
 
-  return httpRequest({
-    'url'  : uri + '/calendars',
-    'json' : true,
-    'qs'   : {'page' : page}
-  }, function (error, res, body) {
-    next(error, body);
+  var lastPage, results, lastLength;
+  results = [];
+  lastLength = 0;
+  lastPage = 0;
+  async.doWhilst(function (next) {
+    return httpRequest({
+      'url'  : uri + url,
+      'json' : true,
+      'qs'   : {'page' : lastPage}
+    }, function (error, res, body) {
+      if (error) {
+        return next(error);
+      }
+      results = results.concat(body);
+      lastLength = body.length;
+      lastPage++;
+      return next();
+    });
+  }, function () {
+    return lastLength > 0;
+  }, function (error) {
+    return next(error, results);
   });
+}
+
+exports.calendars = function (next) {
+  'use strict';
+
+  return downloadPages('/calendars', next);
 };
 
 exports.calendar = function (id, next) {
@@ -32,16 +54,10 @@ exports.calendar = function (id, next) {
   });
 };
 
-exports.events = function (calendar, page, next) {
+exports.events = function (calendar, next) {
   'use strict';
 
-  return httpRequest({
-    'url'  : uri + '/calendars/' + calendar + '/events',
-    'json' : true,
-    'qs'   : {'page' : page}
-  }, function (error, res, body) {
-    next(error, body);
-  });
+  return downloadPages('/calendars/' + calendar + '/events', next);
 };
 
 exports.event = function (calendar, id, next) {
